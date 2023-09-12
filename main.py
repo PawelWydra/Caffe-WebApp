6552a8e2c17a20679ae0147d90ca94d24e0a93a5
Pawel <wydrapawel@icloud.com>
2023-09-12 18:07:45 +0200

    update imports

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 import random
 
@@ -23,9 +23,10 @@ class NewCafe(FlaskForm):
 app = Flask(__name__)
 
 # Connect to Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
 
 # Cafe TABLE Configuration
 class Cafe(db.Model):
@@ -42,33 +43,36 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=True)
 
     def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        return {
+            column.name: getattr(self, column.name) for column in self.__table__.columns
+        }
 
 
 @app.route("/")
 def home():
     cafes = db.session.query(Cafe).all()
-    rnd_numb = random.randint(1, 6)
-    return render_template("index.html", all_cafes=cafes, rnd_numb=rnd_numb )
+    return render_template("index.html", all_cafes=cafes)
 
 
 @app.route("/random")
 def get_random_caffe():
     cafes = db.session.query(Cafe).all()
     random_cafe = random.choice(cafes)
-    return jsonify(cafe={
-        "id": random_cafe.id,
-        "name": random_cafe.name,
-        "map_url": random_cafe.map_url,
-        "img_url": random_cafe.img_url,
-        "location": random_cafe.location,
-        "seats": random_cafe.seats,
-        "has_toilet": random_cafe.has_toilet,
-        "has_wifi": random_cafe.has_wifi,
-        "has_sockets": random_cafe.has_sockets,
-        "can_take_calls": random_cafe.can_take_calls,
-        "coffee_price": random_cafe.coffee_price
-    })
+    return jsonify(
+        cafe={
+            "id": random_cafe.id,
+            "name": random_cafe.name,
+            "map_url": random_cafe.map_url,
+            "img_url": random_cafe.img_url,
+            "location": random_cafe.location,
+            "seats": random_cafe.seats,
+            "has_toilet": random_cafe.has_toilet,
+            "has_wifi": random_cafe.has_wifi,
+            "has_sockets": random_cafe.has_sockets,
+            "can_take_calls": random_cafe.can_take_calls,
+            "coffee_price": random_cafe.coffee_price,
+        }
+    )
 
 
 @app.route("/all")
@@ -82,7 +86,9 @@ def get_cafe_at_location():
     query_location = request.args.get("loc")
     cafes = Cafe.query.filter_by(location=query_location).all()
     if len(cafes) == 0:
-        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+        return jsonify(
+            error={"Not Found": "Sorry, we don't have a cafe at that location."}
+        )
     else:
         return jsonify(cafe=[cafe.to_dict() for cafe in cafes])
 
@@ -107,8 +113,14 @@ def post_new_cafe():
         db.session.commit()
         return jsonify(response={"Success": "Successfully added the new cafe."}), 200
     else:
-        return jsonify(
-            response={"Not Found": "Sorry, that's not allowed. Make sure you have the corretct api_key."}), 404
+        return (
+            jsonify(
+                response={
+                    "Not Found": "Sorry, that's not allowed. Make sure you have the corretct api_key."
+                }
+            ),
+            404,
+        )
 
 
 @app.route("/update-price/<int:cafe_id>", methods=["PATCH"])
@@ -122,7 +134,14 @@ def patch_new_price(cafe_id):
         return jsonify(response={"success": "Successfully updated the price."}), 200
     else:
         # 404 = Resource not found
-        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+        return (
+            jsonify(
+                error={
+                    "Not Found": "Sorry a cafe with that id was not found in the database."
+                }
+            ),
+            404,
+        )
 
 
 @app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
@@ -134,12 +153,31 @@ def delete_caffe(cafe_id):
         if cafe:
             db.session.delete(cafe)
             db.session.commit()
-            return jsonify(response={"Success": "Successfully deleted cafe from database."}), 200
+            return (
+                jsonify(
+                    response={"Success": "Successfully deleted cafe from database."}
+                ),
+                200,
+            )
         else:
-            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database"}), 404
+            return (
+                jsonify(
+                    error={
+                        "Not Found": "Sorry a cafe with that id was not found in the database"
+                    }
+                ),
+                404,
+            )
 
     else:
-        return jsonify(error={"Not Found": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 404
+        return (
+            jsonify(
+                error={
+                    "Not Found": "Sorry, that's not allowed. Make sure you have the correct api_key."
+                }
+            ),
+            404,
+        )
 
 
 @app.route("/selected_coffee/<int:cafe_id>", methods=["POST", "GET"])
@@ -150,7 +188,7 @@ def selected_coffee(cafe_id):
 
 @app.route("/new_cafe", methods=["POST", "GET"])
 def new_cafe():
-    form = NewCafe(meta={'csrf': False})
+    form = NewCafe(meta={"csrf": False})
     if form.validate_on_submit():
         new_cafe_add = Cafe(
             name=request.form.get("name"),
@@ -170,5 +208,5 @@ def new_cafe():
     return render_template("new_cafe.html", form=form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
